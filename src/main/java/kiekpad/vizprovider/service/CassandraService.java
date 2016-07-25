d5e9f5a8ecb4fc30b6e2c406f83d3ec8b7bba111
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-07-25 15:42:15 +0200

    instantiate CassandraService even if no server is available

--- src/main/java/kiekpad/vizprovider/service/CassandraService.java
+++ src/main/java/kiekpad/vizprovider/service/CassandraService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
 
 @Service
 public class CassandraService {
@@ -12,11 +13,15 @@ public class CassandraService {
 	private static final int PORT = 32770;
 	private static final String KEYSPACE = "demo3";
 
-	private final Session session;
+	private Session session; // TODO final
 
 	public CassandraService() {
-		final Cluster cluster = Cluster.builder().addContactPoint(IP_ADDRESS).withPort(PORT).build();
-		this.session = cluster.connect(KEYSPACE);
+		try {
+			final Cluster cluster = Cluster.builder().addContactPoint(IP_ADDRESS).withPort(PORT).build();
+			this.session = cluster.connect(KEYSPACE);
+		} catch (NoHostAvailableException exception) {
+			this.session = null;
+		}
 	}
 
 	public Session getSession() {
