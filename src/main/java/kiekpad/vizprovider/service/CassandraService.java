347d53f2df20f0b3d8c8e85e1b76c2f3c6bd8641
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-07-26 16:35:58 +0200

    updated keyspace

--- src/main/java/kiekpad/vizprovider/service/CassandraService.java
+++ src/main/java/kiekpad/vizprovider/service/CassandraService.java
@@ -12,7 +12,7 @@ public class CassandraService {
 
 	private static final String IP_ADDRESS = "192.168.99.100";
 	private static final int PORT = 32770;
-	private static final String KEYSPACE = "demo3";
+	private static final String KEYSPACE = "Kiekpad";
 
 	private Session session; // TODO final
 
